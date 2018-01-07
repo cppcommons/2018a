de4b3c9169ebd8c144c217e53000dbbb4d02bd9f
root <root@super-computer>
2018-01-08 07:37:52 +0900

    @

--- my-git-setup.bat
+++ my-git-setup.bat
@@ -2,7 +2,7 @@ git config --system core.autoCRLF false
 git config --system user.name  root
 git config --system user.email root@super-computer
 git config --unset-all credential.helper
-git config --system --unset-all credential.helper
-git config --system credential.helper manager
-git config --system credential.useHttpPath true
+git config --global --unset-all credential.helper
+git config --global credential.helper manager
+git config --global credential.useHttpPath true
 pause
